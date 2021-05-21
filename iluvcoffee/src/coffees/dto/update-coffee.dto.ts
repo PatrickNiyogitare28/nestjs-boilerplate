f2035686e618a9c664b50e367ac12a9ec433c1b6
PatrickNiyogitare28 <patrickniyogitare28@gmail.com>
2021-05-21 21:32:00 +0200

    chore: added pipes

--- iluvcoffee/src/coffees/dto/update-coffee.dto.ts
+++ iluvcoffee/src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,7 @@
-export class UpdateCoffeeDto {
-    readonly name?: string;
-    readonly brand?:string;
-    readonly flavors?:string[];
-}
+import {PartialType} from '@nestjs/swagger';
+import { CreateCoffeeDto } from './create-coffee.dto';
+/**
+ Role: Partial types imported from @nestjs/mapped-types package provide us with access to all properties in
+ CreateCoffeeDto and are used as optional
+*/
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
